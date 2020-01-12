bab98370b25c2d77d253314a1ccb8adddd78e70b
Angad Singh <mail@angad.dev>
2020-01-13 00:55:30 +0530

    Done!

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const asyncH = require("express-async-handler");
+const Spreadsheet = require("google-spreadsheet");
+const { promisify } = require("util");
 
-const port = 5000;
+const creds = require("./client-secret.json");
+const app = express();
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+async function getSheet(n, doc) {
+  // Authenticate with the Google Spreadsheets API.
+  await promisify(doc.useServiceAccountAuth)(creds);
+  const info = await promisify(doc.getInfo)();
+  const sheet = info.worksheets[n];
+  return sheet;
+}
 
 app.post(
-  "/",
+  "/:sheetId",
   asyncH(async (req, res) => {
-    res.json(req.body);
+    try {
+      const ref = req.header("Referer");
+      const { sheetId } = req.params;
+      const doc = new Spreadsheet(sheetId);
+      const sheet = await getSheet(0, doc);
+      await promisify(sheet.addRow)(req.body);
+
+      res.redirect(ref, 200);
+    } catch (err) {
+      console.log(err);
+      res.redirect(ref, 500);
+    }
   })
 );
 
-// Listen on port 5000
-app.listen(port, () =>
+app.listen(5000, () =>
   console.log(`Server is booming on port 5000
 Visit http://localhost:5000`)
 );
